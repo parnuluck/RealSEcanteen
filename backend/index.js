0b6c8248749848ca1f7db08c5e55feac37091c47
parnuluck <parnuluck.s@gmail.com>
2026-04-01 14:18:06 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-/*app.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("API running ✅");
-});*/
+});
 
  // 🔑 config
 const SECRET = "mySuperSecret123";
