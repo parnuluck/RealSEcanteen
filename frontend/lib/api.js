da99e4562439a6465dfe91af3810a9192dc2dcf8
parnuluck <parnuluck.s@gmail.com>
2026-04-01 20:51:45 +0700

    Update api.js

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -3,9 +3,8 @@ import axios from "axios";
 const API = process.env.NEXT_PUBLIC_API_URL;
 
 const api = axios.create({
-  baseURL: API
+  baseURL: "https://realsecanteen-3.onrender.com"
 });
-
 // 🔥 ใส่ token อัตโนมัติทุก request
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
