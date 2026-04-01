6ddc55c10f9634c9eb37ad7982a498a3485ad7ac
parnuluck <parnuluck.s@gmail.com>
2026-04-02 00:08:48 +0700

    Update page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -392,12 +392,12 @@ export default function Home() {
         )}
       </div>
 
-      <button
+      /*<button
         onClick={myQueue}
         className="bg-blue-500 text-white px-4 py-2 mt-2 rounded"
       >
         Check Queue
-      </button>
+      </button>*/
 
       <button
         onClick={startScan}
