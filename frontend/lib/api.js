d4a876274ef38378f209d1c138536cbdfcf6901c
parnuluck <parnuluck.s@gmail.com>
2026-04-01 18:36:46 +0700

    Update api.js

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,1 +1,20 @@
-export const API = process.env.NEXT_PUBLIC_API_URL;
+import axios from "axios";
+
+const API = process.env.NEXT_PUBLIC_API_URL;
+
+const api = axios.create({
+  baseURL: API
+});
+
+// 🔥 ใส่ token อัตโนมัติทุก request
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default api;
